2b4a5b28c42ed230a696447e254dd30e8ae5a3c2
ormxmi <36085168+ormxmi@users.noreply.github.com>
2018-10-26 01:58:33 +0300

    design

---
+++ main.js
@@ -0,0 +1,4 @@
+function startGame(){
+    document.getElementsByClassName("enter-name-screen")[0].style.display = "none";
+    document.getElementsByTagName("body")[0].style.background = "lightblue";
+}
