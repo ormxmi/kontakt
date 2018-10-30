953f4266097a529a39c31d68880ae64ca4872510
ormxmi <36085168+ormxmi@users.noreply.github.com>
2018-10-30 13:10:45 +0200

    wip

--- main.js
+++ main.js
@@ -51,3 +51,16 @@ function countDown(seconds){
         
     },1010)
 }
+var displayActivePlayersClicked = false;
+function displayActivePlayers(){
+    if(!displayActivePlayersClicked){
+        document.getElementsByClassName("activePlayers")[0].style.transform = "translateX(calc(0% - 10vw))";
+        document.getElementById("leftArrow").style.transform = "rotate(-180deg)";
+        displayActivePlayersClicked = true;
+    }
+    else{
+        document.getElementsByClassName("activePlayers")[0].style.transform = "translateX(calc(-100% - 10vw))";
+        document.getElementById("leftArrow").style.transform = "rotate(0deg)";
+        displayActivePlayersClicked = false;
+    }
+}
