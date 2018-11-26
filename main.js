8e36771cba1fa29a1d34e1a163be2fd6678a7400
ormxmi <36085168+ormxmi@users.noreply.github.com>
2018-11-26 23:37:30 +0200

    nth

--- main.js
+++ main.js
@@ -57,10 +57,37 @@ function displayActivePlayers(){
         document.getElementsByClassName("activePlayers")[0].style.transform = "translateX(calc(0% - 10vw))";
         document.getElementById("leftArrow").style.transform = "rotate(-180deg)";
         displayActivePlayersClicked = true;
+        if(displayUsedWords){
+            setTimeout(function(){
+            document.getElementsByClassName("used-words-container")[0].style.transform = "translateX(calc(100% + 10vw))";
+            document.getElementById("rightArrow").style.transform = "rotate(0deg)";
+            displayUsedWords = false;
+            },70)
+        }
     }
     else{
         document.getElementsByClassName("activePlayers")[0].style.transform = "translateX(calc(-100% - 10vw))";
         document.getElementById("leftArrow").style.transform = "rotate(0deg)";
         displayActivePlayersClicked = false;
+    }
+}
+var displayUsedWords = false;
+function displayUsedWord(){
+    if(!displayUsedWords){
+        document.getElementsByClassName("used-words-container")[0].style.transform = "translateX(calc(0% + 10vw))";
+        document.getElementById("rightArrow").style.transform = "rotate(-180deg)";
+        displayUsedWords = true;
+        if(displayActivePlayersClicked){
+            setTimeout(function(){
+            document.getElementsByClassName("activePlayers")[0].style.transform = "translateX(calc(-100% - 10vw))";
+            document.getElementById("leftArrow").style.transform = "rotate(0deg)";
+            displayActivePlayersClicked = false;
+            },70)
+        }
+    }
+    else{
+        document.getElementsByClassName("used-words-container")[0].style.transform = "translateX(calc(100% + 10vw))";
+        document.getElementById("rightArrow").style.transform = "rotate(0deg)";
+        displayUsedWords = false;
     }
 }
